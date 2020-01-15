77e6dcca2b1ee98594d919a5c17ffe0ad14d9e7d
Jason Blanchard <jas.blanchard@gmail.com>
2020-01-14 20:39:35 -0700

    Add csrf token

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import exphbs from 'express-handlebars';
+import jwt from 'jsonwebtoken';
 
 interface Manifest {
   [key: string]: string;
@@ -21,6 +22,13 @@ const PAGE_TITLE = process.env.PAGE_TITLE || 'App';
 const APP_CONTAINER_ID = process.env.APP_CONTAINER_ID || 'app';
 const jsSource = `${BASE_PATH}${JS_BUNDLE_PATH}`;
 
+function getAuthorizationToken(authorizationHeader: string | undefined) {
+  if (!authorizationHeader) return '';
+  const match = authorizationHeader.match(/^Bearer (.+)$/);
+  if (!match) return '';
+  return match[1];
+}
+
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -30,12 +38,21 @@ app.get('/health', (_request, response) => {
   return response.json({ health: 'ok' });
 });
 
-app.get('*', (_request, response) => {
+interface DecodedAuthorizationPayload {
+  csrfToken: string;
+}
+
+app.get('*', (request, response) => {
+  const token = getAuthorizationToken(request.headers.authorization);
+  const decodedToken = jwt.decode(token);
+  const csrfToken = decodedToken ? (decodedToken as DecodedAuthorizationPayload).csrfToken : '';
+
   response.render('index', {
     layout: false,
     title: PAGE_TITLE,
     appContainerId: APP_CONTAINER_ID,
     jsSource,
+    csrfToken,
   });
 });
 
